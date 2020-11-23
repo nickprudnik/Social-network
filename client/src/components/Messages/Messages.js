75ae8ab009a761ec8797bef1445b797b4db150fc
nickprudnik <prudniknick@gmail.com>
2020-11-23 10:43:10 +0300

    remove unnecessary packages

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Messages.css';
-import Message from '../Message/Message';
+import Message from './Message/Message';
 
 import ScrollToBottom from 'react-scroll-to-bottom';
 
